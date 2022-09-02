e58e48be793a0666d018d08be8cbe3238be0e60d
Коротченков Костя <kostyak127@gmail.com>
2022-09-02 20:40:59 +0500

    remove unused files

--- src/cron/services/bree-cron.service.ts
+++
@@ -1,7 +0,0 @@
-import { Injectable } from "@nestjs/common";
-import { DiscoveryService } from "@nestjs/core";
-
-@Injectable()
-export class BreeCronService {
-  public constructor(private readonly discoveryService: DiscoveryService) {}
-}
